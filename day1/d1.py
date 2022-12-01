a800a312eb1102dcb66083e39c89758aab15ca78
hatch <hatch@quakebag.com>
2022-12-01 07:34:52 +0100

    day1

---
+++ day1/d1.py
@@ -0,0 +1,23 @@
+def calc(inputs):
+    s = 0
+    all_sums = []
+    for x in inputs:
+        if x == '':
+            all_sums.append(s)
+            s = 0
+        else:
+            s += int(x)
+
+    print(sorted(all_sums)[-3:])
+    print(sum(sorted(all_sums)[-3:]))
+    print('\n')
+
+
+def main():
+    with open('day1.txt') as data:
+        inputs = data.read().splitlines()
+        calc(inputs)
+
+
+if __name__ == '__main__':
+    main()
