ed545e389c77007f32e1dbfe27cc8e24dab08b79
hatch <hatch@quakebag.com>
2022-12-06 08:25:09 +0100

    day 6

---
+++ day6/p2.py
@@ -0,0 +1,27 @@
+
+def has_duplicated_chars(s: str) -> bool:
+    char_sum = 0
+    for ch in list(s):
+        for ch2 in list(s):
+            if ch == ch2:
+                char_sum += 1
+    return char_sum > len(s)
+
+
+def get_marker_index(line: str) -> int:
+    for idx in list(range(0, len(line) - 13)):
+        if has_duplicated_chars(line[idx:idx+14]):
+            continue
+        return idx + 14
+
+
+def main():
+    with open('day6.txt') as data:
+        lines = data.read().splitlines()
+        for line in lines:
+            result = get_marker_index(line)
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
